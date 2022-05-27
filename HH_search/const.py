df6abb52b175909743a5164e37412270cf6b7c31
valvandor <sincov.vitalii@gmail.com>
2022-05-27 15:28:57 +0300

    refactor: add constants for hh search
    store json files in a shared directory named data

---
+++ HH_search/const.py
@@ -0,0 +1,7 @@
+
+FILE_EXTENSION = '_vacancies.json'
+
+#  should be for mongo
+ROOT_DIRECTORY = '../'
+DATA_DIRECTORY = 'data/'
+LAST_SEARCHED_TEXT = 'last_searched_text.json'
--- HH_search/main.py
+++ HH_search/main.py
@@ -1,16 +1,33 @@
 import json
+import os
 
-from hh_search import make_fully_hh_search_by_word
+from HH_search import const
+from hh_search import make_fully_hh_search
 
-search_word = 'python'
+searching_for = 'python'
 
-file_path = f'{search_word}_vacancies.json'
 
-with open(file_path, 'w', encoding='utf-8') as file:
-    json.dump(
-        make_fully_hh_search_by_word(search_word), file)
+def main(text):
+    if not os.path.exists(f'{const.ROOT_DIRECTORY}{const.DATA_DIRECTORY}'):
+        os.mkdir(f'{const.ROOT_DIRECTORY}{const.DATA_DIRECTORY}')
 
-answer = input('\nРаспечатать количество полученных вакансий? (press Enter to deny): ')
-if answer:
-    with open(file_path, 'r') as file:
-        print(len(json.load(file)))
+    text = text.strip()
+    searched = text.replace(' ', '_')
+
+    json_file = f'{const.ROOT_DIRECTORY}{const.DATA_DIRECTORY}{searched}{const.FILE_EXTENSION}'
+    with open(json_file, 'w', encoding='utf-8') as file:
+        json.dump(
+            make_fully_hh_search(text), file)
+
+    searched_text_file = f'{const.ROOT_DIRECTORY}{const.DATA_DIRECTORY}{const.LAST_SEARCHED_TEXT}'
+    with open(searched_text_file, 'w', encoding='utf-8') as file:
+        json.dump(text, file)
+
+    answer = input('\nРаспечатать количество полученных вакансий? (press Enter to deny): ')
+    if answer:
+        with open(json_file, 'r') as file:
+            print(len(json.load(file)))
+
+
+if __name__ == '__main__':
+    main(searching_for)
