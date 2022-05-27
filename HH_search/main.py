e8e14fb3cba8487171e9eca5442f632525dd5292
valvandor <sincov.vitalii@gmail.com>
2022-05-27 10:39:34 +0300

    fix: correct file name for reading attempt

--- HH_search/main.py
+++ HH_search/main.py
@@ -5,11 +5,13 @@ from hh_search import make_fully_hh_search_by_word
 
 search_word = 'python'
 
-with open(f'{search_word}_vacancies.json', 'w', encoding='utf-8') as file:
+file_path = f'{search_word}_vacancies.json'
+
+with open(file_path, 'w', encoding='utf-8') as file:
     json.dump(
         make_fully_hh_search_by_word(search_word), file)
 
 answer = input('Распечатать? (press Enter to deny)\n')
 if answer:
-    with open('vacancies.json', 'r') as file:
+    with open(file_path, 'r') as file:
         pprint(json.load(file))
