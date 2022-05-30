b57b64812e61c4ae9aeb7c868a7f4c679d20da5f
valvandor <sincov.vitalii@gmail.com>
2022-05-30 10:33:54 +0300

    fix: correct cache dir path

--- HH_search/helpers.py
+++ HH_search/helpers.py
@@ -15,9 +15,9 @@ def make_cache_dir(keyword, folder_name):
     """
     Makes directories for storing loaded pages
     """
-    if not os.path.exists(f'../{const.CACHE_DIR_NAME}'):
-        os.mkdir(f'../{const.CACHE_DIR_NAME}')
-    dir_with_pages = f'../{const.CACHE_DIR_NAME}"{keyword}" {folder_name}/'
+    if not os.path.exists(f'{const.ROOT_DIRECTORY}{const.CACHE_DIR_NAME}'):
+        os.mkdir(f'{const.ROOT_DIRECTORY}{const.CACHE_DIR_NAME}')
+    dir_with_pages = f'{const.ROOT_DIRECTORY}{const.CACHE_DIR_NAME}"{keyword}" {folder_name}/'
     if not os.path.exists(dir_with_pages):
         os.mkdir(dir_with_pages)
     return dir_with_pages
--- const.py
+++ const.py
@@ -3,7 +3,7 @@ Constants for manipulate with files for storing
 """
 #  should be for mongo
 FILE_EXTENSION = '_vacancies.json'
-ROOT_DIRECTORY = '../'
+ROOT_DIRECTORY = './'
 DATA_DIRECTORY = 'data/'
 CACHE_DIR_NAME = 'cache/'
 FILE_LAST_SEARCHED_TEXT = 'last_searched_text.json'
