875fda5462108ab6c6656a4cd7db8edb1874a970
valvandor <sincov.vitalii@gmail.com>
2022-05-30 14:53:34 +0300

    chore: leave a comment for purpose of constant

--- const.py
+++ const.py
@@ -3,7 +3,7 @@ Constants for manipulate with files for storing
 """
 #  should be for mongo
 FILE_EXTENSION = '_vacancies.json'
-ROOT_DIRECTORY = './'
+ROOT_DIRECTORY = './'  # relative path from place, where search method of HH search service is called
 DATA_DIRECTORY = 'data/'
 CACHE_DIR_NAME = 'cache/'
 FILE_LAST_SEARCHED_TEXT = 'last_searched_text.json'
