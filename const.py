66fac75e21b857cfb5865b27dda0caf0bda37122
valvandor <sincov.vitalii@gmail.com>
2022-06-03 22:47:45 +0300

    refactor: simplify storing service

--- const.py
+++ const.py
@@ -4,16 +4,13 @@ Constants for manipulate with files for storing
 
 DB_NAME = 'vacancies_searching'
 SEARCHED_COLLECTION = 'collection_searched_text'
-SEARCHED_TEXT_KEY = 'searched_text'
 
-FILE_PATHS_CONST = {
-    'root_directory': './',  # relative path from place, where search method of HH search service is called
-    'data_directory': 'data/',
-    'cache_directory': 'cache/',
-    'json_file_suffix': '_vacancies.json',
-    'searched_text_key': SEARCHED_TEXT_KEY,
-    'file_containing_last': 'last_searched_text.json',
-}
+ROOT_DIRECTORY = './'  # relative path from place, where search method of HH search service is called
+DATA_DIRECTORY = 'data/'
+CACHE_DIRECTORY = 'cache/'
+JSON_SUFFIX_FILE = '_vacancies.json'
+SEARCHED_TEXT_KEY = 'searched_text'
+FILE_CONTAINING_LAST = 'last_searched_text.json'
 
 STORING_CONST = {
     'id': '_id',
